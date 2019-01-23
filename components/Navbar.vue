5b63a71e1f2fa791cf8de724f73540dd86edda1d
Zach Caldwell <zachary.austin.caldwell@gmail.com>
2019-01-23 18:38:53 -0500

    start navbar work

---
+++ components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <header class="header_area">
+    <div class="main_menu">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <nuxt-link to="/" class="navbar-brand logo_h">
+            <img src="~/assets/img/website-logo1.png" alt>
+          </nuxt-link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+            <ul class="nav navbar-nav menu_nav justify-content-center">
+              <nuxt-link tag="li" to="/" class="nav-item" exact>
+                <a class="nav-link">Home</a>
+              </nuxt-link>
+              <nuxt-link tag="li" to="/about" class="nav-item">
+                <a class="nav-link">About</a>
+              </nuxt-link>
+              <nuxt-link tag="li" to="/gallery" class="nav-item">
+                <a class="nav-link">Gallery</a>
+              </nuxt-link>
+              <nuxt-link tag="li" to="/blog" class="nav-item">
+                <a class="nav-link">Blog</a>
+              </nuxt-link>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li class="nav-item">
+                <a href="#" class="primary_btn">join us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
+
