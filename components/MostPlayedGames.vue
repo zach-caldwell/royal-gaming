be6b519452b76e04c70f5bdd4705ff88eb85071e
Zach Caldwell <zachary.austin.caldwell@gmail.com>
2019-02-05 10:51:14 -0500

    Build Most Played Games section

---
+++ components/MostPlayedGames.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="upcoming_games_area">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="main_title">
+            <h2>Most Played Games</h2>
+            <h1>Most Played Games</h1>
+          </div>
+        </div>
+      </div>
+      <div class="row text-center">
+        <div class="col-lg-3 col-md-6 col-sm-6">
+          <div class="new_games_item shadow">
+            <img src="~assets/img/mpg_rl_final.png" alt>
+            <div class="upcoming_title">
+              <h3>Rocket League</h3>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6">
+          <div class="new_games_item shadow">
+            <img src="~assets/img/mpg_teso_final.png" alt>
+            <div class="upcoming_title">
+              <h3>The Elder Scrolls Online</h3>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6">
+          <div class="new_games_item shadow">
+            <img src="~assets/img/mpg_fn_final.png" alt>
+            <div class="upcoming_title">
+              <h3>Fortnite</h3>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6">
+          <div class="new_games_item shadow">
+            <img src="~assets/img/mpg_csgo_final.png" alt>
+            <div class="upcoming_title">
+              <h3>Counter-Strike: Global Offensive</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,20 @@
   <div>
     <HomeBanner/>
     <WhyUs/>
+    <MostPlayedGames/>
   </div>
 </template>
 
 <script>
 import HomeBanner from "../components/HomeBanner";
 import WhyUs from "../components/WhyUs";
+import MostPlayedGames from "../components/MostPlayedGames";
 
 export default {
   components: {
     HomeBanner,
-    WhyUs
+    WhyUs,
+    MostPlayedGames
   }
 };
 </script>
