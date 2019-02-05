00959fa809582bf8eba1a63bfc782a0e3cd2b42d
Zach Caldwell <zachary.austin.caldwell@gmail.com>
2019-02-05 11:51:48 -0500

    Build footer baseline

---
+++ components/Footer.vue
@@ -0,0 +1,41 @@
+<template>
+  <footer class="footer_area section_gap_top">
+    <div class="container">
+      <div class="row single-footer-widget">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <div class="copy_right_text">
+            <p>
+              Copyright &copy; {{ getYear }} All rights reserved
+              <br>UI template courtesy of
+              <a href="https://colorlib.com" target="_blank">Colorlib</a>
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <div class="social_widget">
+            <a href="#">
+              <i class="fab fa-discord"></i>
+            </a>
+            <a href="#">
+              <i class="fab fa-twitch"></i>
+            </a>
+            <a href="#">
+              <i class="fab fa-youtube"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  computed: {
+    getYear: function currentYear() {
+      return new Date().getFullYear();
+    }
+  }
+};
+</script>
+
